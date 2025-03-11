d59a13ff5b4c0463846129f6de8c3cf557d2d1ed
pqud <49116761+pqud@users.noreply.github.com>
2025-03-11 11:43:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -108,5 +108,9 @@ def api_valid():
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
     
 
+@app.route('/Problems')
+def problem():
+    return render_template('Problems.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
